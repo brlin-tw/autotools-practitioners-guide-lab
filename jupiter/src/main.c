9844ba7be355cd88d2b609a6e10b6023a9d693f1
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2025-03-01 23:36:19 +0800

    feat(ch2): Implement basic jupiter project
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

---
+++ jupiter/src/main.c
@@ -0,0 +1,14 @@
+/*
+    The main entrypoint of the juniper program
+    Copyright 2008 John Calcote <http://freesoftwaremagazine.com/authors/John%20Calcote>
+    Copyright 2025 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
+    SPDX-License-Identifier: WTFPL
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+    printf("Hello from %s!\n", argv[0]);
+    return 0;
+}
